e1406479a818eda49a8e2e658149d264513bd884
arhamm-2005 <153810088+arhamm-2005@users.noreply.github.com>
2024-11-09 13:04:05 +0500

    Create Lab1_Task2.java

---
+++ Lab1_Task2.java
@@ -0,0 +1,17 @@
+
+package javaapplication23;
+
+public class JavaApplication23 {
+
+
+    public static void main(String[] args) {
+        double primDouble = 25.75;
+        
+        // Converting primitive double to Wrapper Double
+        Double wrappedDouble = primDouble;
+        
+        System.out.println("Primitive Double: " + primDouble);
+        System.out.println("Wrapper Double: " + wrappedDouble);
+    }
+    
+}
