7ad8650998b0ecc747421e29656268762bd3a374
arhamm-2005 <153810088+arhamm-2005@users.noreply.github.com>
2024-11-09 13:06:23 +0500

    Create Lab1_Task3.java

---
+++ Lab1_Task3.java
@@ -0,0 +1,25 @@
+
+package javaapplication23;
+
+public class JavaApplication23 {
+
+
+    public static void main(String[] args) {
+         String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "Java";
+        String str4 = "Programming";
+        String str5 = "Language";
+        
+        // Concatenating all strings
+        String concatenated = str1 + " " + str2 + " " + str3 + " " + str4 + " " + str5;
+        System.out.println("Concatenated String: " + concatenated);
+        
+        // Converting fourth string to uppercase
+        System.out.println("Uppercase of fourth string: " + str4.toUpperCase());
+        
+        // Substring from 8th index onward
+        System.out.println("Substring from index 8: " + concatenated.substring(8));
+    }
+    
+}
