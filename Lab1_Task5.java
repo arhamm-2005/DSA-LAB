0c6a5bea6f5cab9022b3deb57ffca1b2c410c4f8
arhamm-2005 <153810088+arhamm-2005@users.noreply.github.com>
2024-11-09 13:10:14 +0500

    Create Lab1_Task5.java

---
+++ Lab1_Task5.java
@@ -0,0 +1,21 @@
+
+package javaapplication23;
+
+public class JavaApplication23 {
+
+
+    public static void main(String[] args) {
+         // Minimum and Maximum values of Integer
+        System.out.println("Integer Minimum Value: " + Integer.MIN_VALUE);
+        System.out.println("Integer Maximum Value: " + Integer.MAX_VALUE);
+
+        // Minimum and Maximum values of Float
+        System.out.println("Float Minimum Value: " + Float.MIN_VALUE);
+        System.out.println("Float Maximum Value: " + Float.MAX_VALUE);
+
+        // Minimum and Maximum values of Double
+        System.out.println("Double Minimum Value: " + Double.MIN_VALUE);
+        System.out.println("Double Maximum Value: " + Double.MAX_VALUE);
+    }
+    
+}
