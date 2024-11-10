8221b798d9bb70b3e587b172c9a4090e3617a6f6
arhamm-2005 <153810088+arhamm-2005@users.noreply.github.com>
2024-11-10 16:41:19 +0500

    Create Lab2_HomeTask4.java

---
+++ Lab2_HomeTask4.java
@@ -0,0 +1,39 @@
+package javaapplication23;
+import java.util.*;
+
+class Color {
+    int red, green, blue;
+
+    public Color(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Color color = (Color) o;
+        return red == color.red && green == color.green && blue == color.blue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(red, green, blue);
+    }
+
+    @Override
+    public String toString() {
+        return "Color(R:" + red + ", G:" + green + ", B:" + blue + ")";
+    }
+}
+public class JavaApplication23 {
+     public static void main(String[] args) {
+        Color color1 = new Color(255, 0, 0);
+        Color color2 = new Color(255, 0, 0);
+
+        System.out.println("Are the colors equal? " + color1.equals(color2));
+         
+    }
+}
