fcaf6e7e866dfa0f1032da770f62b5d22c8045e7
arhamm-2005 <153810088+arhamm-2005@users.noreply.github.com>
2024-11-10 16:36:28 +0500

    Create Lab2_HomeTask1.java

---
+++ Lab2_HomeTask1.java
@@ -0,0 +1,20 @@
+package javaapplication23;
+import java.util.*;
+
+public class JavaApplication23 {
+     public static void main(String[] args) {
+        Vector<Integer> numbers = new Vector<>(Arrays.asList(4, 2, 8, 5, 1, 9));
+
+        // Sort the Vector
+        Collections.sort(numbers);
+
+        // Display the largest and smallest numbers
+        int largest = Collections.max(numbers);
+        int smallest = Collections.min(numbers);
+
+        System.out.println("Sorted Vector: " + numbers);
+        System.out.println("Largest Number: " + largest);
+        System.out.println("Smallest Number: " + smallest);
+         
+    }
+}
