623a7980cdb5995a5feb353fab483d5ad09c6019
arhamm-2005 <153810088+arhamm-2005@users.noreply.github.com>
2024-11-10 16:39:33 +0500

    Create Lab2_HomeTask3.java

---
+++ Lab2_HomeTask3.java
@@ -0,0 +1,42 @@
+package javaapplication23;
+import java.util.*;
+
+class Employee {
+    String name;
+    int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && name.equals(employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (ID: " + id + ")";
+    }
+}
+public class JavaApplication23 {
+     public static void main(String[] args) {
+        HashSet<Employee> employees = new HashSet<>();
+
+        employees.add(new Employee("Faiz", 101));
+        employees.add(new Employee("Ashar", 102));
+        employees.add(new Employee("Arham", 101)); // Duplicate entry
+
+        System.out.println("Employees:\n" + employees);
+         
+    }
+}
