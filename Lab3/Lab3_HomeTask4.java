868cf4d058f4c695840026c32ed8e62563acde90
arhamm-2005 <153810088+arhamm-2005@users.noreply.github.com>
2024-11-11 16:23:13 +0500

    Create Lab3_HomeTask4.java

---
+++ Lab3/Lab3_HomeTask4.java
@@ -0,0 +1,21 @@
+package javaapplication23;
+import java.util.*;
+
+
+public class JavaApplication23 {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;  // Base case: All characters have been checked
+        }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;  // If characters don't match, not a palindrome
+        }
+        return isPalindrome(str, start + 1, end - 1);  // Recursive check for next characters
+    }
+     public static void main(String[] args) {
+       String word = "Arham";
+        System.out.println(isPalindrome(word, 0, word.length() - 1) ? 
+         "YES (" +word +") is a PALINDROME" : "NO (" +word +") is not a PALINDROME"); 
+        
+    }
+}
