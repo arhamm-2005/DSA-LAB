d00eef169ea50100d4ba27886f34720cf249ce32
arhamm-2005 <153810088+arhamm-2005@users.noreply.github.com>
2024-11-11 16:14:11 +0500

    Create Lab3_Task6.java

---
+++ Lab3/Lab3_Task6.java
@@ -0,0 +1,17 @@
+package javaapplication23;
+import java.util.*;
+
+
+public class JavaApplication23 {
+    public static int countdigits(int number) {
+        if (number == 0) {
+            return 0;  // Base case: if number is 0, no more digits
+        }
+        return 1 + countdigits(number / 10);  // Recursive call
+    }
+     public static void main(String[] args) {
+        System.out.println("The digits counts of given value is: "
+                +countdigits(53545354));  // Example input 
+        
+    }
+}
