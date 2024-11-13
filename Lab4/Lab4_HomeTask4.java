b501c81811b011e89df6d30a0908ebf487893142
Muhammad Arham Shakeel <153810088+arhamm-2005@users.noreply.github.com>
2024-11-13 10:26:26 +0500

    Create Lab4_HomeTask4.java

---
+++ Lab4/Lab4_HomeTask4.java
@@ -0,0 +1,24 @@
+package javaapplication23;
+import java.util.*;
+
+public class JavaApplication23 {
+
+    public static void main(String[] args) {
+        int[] numbers = {0,1,2,3,5};
+        int missing = findMissingNumber(numbers);
+        
+        System.out.println("The missing number is: " + missing);
+    }
+
+    public static int findMissingNumber(int[] arr) {
+        int n = arr.length;
+        int totalSum = n * (n + 1) / 2; // Sum of numbers from 0 to n
+        int arraySum = 0;
+        
+        for (int num : arr) {
+            arraySum += num;
+        }
+        
+        return totalSum - arraySum;
+    }
+}
