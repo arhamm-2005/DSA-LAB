04d9111dc1ede902a43ea4dcdb03a42eb2171fb6
Muhammad Arham Shakeel <153810088+arhamm-2005@users.noreply.github.com>
2024-11-20 10:25:21 +0500

    Create Lab5_HomeTask3.java

---
+++ Lab5/Lab5_HomeTask3.java
@@ -0,0 +1,53 @@
+package javaapplication23;
+import java.util.*;
+
+public class JavaApplication23 {
+
+    public static void main(String[] args) {
+       String[] arr = {"Arham", "Cricket", "Java", "Atibah", "Youtube"};
+
+        System.out.println("Original Array:");
+        System.out.println(Arrays.toString(arr));
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted Array:");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void mergeSort(String[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(String[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        String[] L = new String[n1];
+        String[] R = new String[n2];
+
+        System.arraycopy(arr, left, L, 0, n1);
+        System.arraycopy(arr, mid + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i].compareTo(R[j]) <= 0) arr[k++] = L[i++];
+            else arr[k++] = R[j++];
+        }
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+}
+
+
+
+
+
+
+
